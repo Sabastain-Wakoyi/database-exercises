8c743600af57d8f08284c0858d529fef2363f7c1
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-25 15:48:06 -0600

    delete exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db;
+
+-- SELECT QUERIES
+SELECT name AS 'Albums released after 1991'
+FROM albums
+WHERE release_date > 1991;
+
+-- Albums with the genre 'disco'
+SELECT name AS 'Albums with the genre ''disco'''
+FROM albums
+WHERE genre LIKE '%disco%';
+
+-- Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+
+SELECT name AS 'Albums by ''Whitney Houston'''
+FROM albums
+WHERE artist LIKE '%whitney houston%';
