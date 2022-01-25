3a5efec0541fad595585769e02835f32e0202442
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-25 14:03:18 -0600

    working on select

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,1 +1,17 @@
 USE codeup_test_db;
+
+
+-- In select_exercises.sql write queries to find the following information.
+-- Before each item, output a caption explaining the results:
+
+-- The name of all albums by Pink Floyd.
+-- SELECT name AS 'Pink Floyd''s albums'
+-- FROM albums
+-- WHERE artist = 'Pink Floyd';
+SELECT 'The name of all albums by Pink Floyd'
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released';
+SELECT release_year FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
