38a4a54463a449eb2853681d3a8e631a61941f26
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-25 14:05:36 -0600

    select exerc

--- select_exercises.sql
+++ select_exercises.sql
@@ -15,3 +15,11 @@ SELECT * FROM albums WHERE artist = 'Pink Floyd';
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
 SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released';
 SELECT release_year FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+-- The genre for Nevermind
+SELECT 'The genre for Nevermind';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+-- Which albums were released in the 1990s
+SELECT 'Which albums were released in the 1990s';
+SELECT * FROM albums WHERE release_year BETWEEN 1990 AND 1999;
