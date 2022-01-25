387989f76b532eddfb66ff113d32b5ee063820c8
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-24 21:25:54 -0600

    finished exercise 1

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+    id      INT UNSIGNED NOT NULL AUTO_INCREAMENT,
+    artist_name VARCHAR(100) NOT NULL,
+    record_name VARCHAR(100) NOT NULL,
+    release_date SMALLINT UNSIGNED,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+
+);
