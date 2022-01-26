2964d7e703eb68369d1a78a58f9de83a32299928
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-26 12:05:27 -0600

    where exer

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,2 +1,10 @@
 USE employees;
 
+-- Create a file named where_exercises.sql.
+-- Make sure to use the employees database before you begin.
+--             Part 1
+
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
