13bd636e78412e1acac7743c29606283ec0373e0
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-26 11:48:50 -0600

    where lec

--- where_lecture.sql
+++ where_lecture.sql
@@ -37,3 +37,10 @@ WHERE emp_no <= 10026;
 SELECT *
 FROM titles
 WHERE to_date IS NOT NULL;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no < 20000
+     AND (last_name IN ('Herber', 'Baek')
+    OR first_name = 'Shridhar'
+    );
