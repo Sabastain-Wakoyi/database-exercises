8f66f7c0c48c71c281f8fe718f40c11f04ead9ea
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-26 12:40:46 -0600

    done with where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -30,3 +30,25 @@ WHERE ( first_name = 'Irena'
    OR first_name = 'Vidya'
    OR first_name = 'Maya')
     AND GENDER = 'M';
+
+# we can also use this solution
+SELECT * FROM employees
+WHERE first_name = 'Irena' AND gender = 'M'
+   OR first_name = 'Vidya' AND gender = 'M'
+   OR first_name = 'Maya' AND gender = 'M';
+
+
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT * FROM employees
+WHERE last_name LIKE 'e%'
+OR last_name LIKE '%e';
+
+#     Duplicate the previous query and update it to find all
+#     employees whose last name starts and ends with 'E' — 899 rows.
+SELECT * FROM employees
+WHERE last_name LIKE 'e%e';
+
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT * FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%q%';
