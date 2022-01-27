4fcab50d04b2ebd2352fd30d0c845775a9d485d7
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-27 15:24:31 -0600

    group by exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -43,3 +43,13 @@ FROM employees
 WHERE last_name like '%q%' and last_name not like '%qu%'
 GROUP BY last_name;
 
+# Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+# 189 Chleq
+# 190 Lindqvist
+# 168 Qiwen
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
