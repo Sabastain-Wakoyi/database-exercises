f9d0d96eb79aa4e6efa4c33e5a8eea01bad30f0b
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-30 20:08:08 -0600

    indexes exercises

---
+++ indexes_exercise.sql
@@ -0,0 +1,26 @@
+USE employees;
+
+-- USE your employees database
+
+-- DESCRIBE each table and inspect the keys and see which columns have indexes and keys
+HOW INDEXES FROM salaries;
+
+DROP INDEX salary_index ON salaries;
+
+SHOW TABLES;
+
+DESCRIBE departments;
+
+SELECT * FROM departments;
+
+DESCRIBE dept_emp;
+
+DESCRIBE dept_manager;
+
+DESCRIBE employees;
+
+DESCRIBE salaries;
+
+DESCRIBE titles;
+
+SELECT * FROM titles;
