8103891e803a6c98013677b028a346f9fbbf7f6f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-30 20:35:04 -0600

    join example

--- join_test_db.sql
+++ join_test_db.sql
@@ -50,3 +50,27 @@ INSERT INTO users (name, email, role_id) VALUES
                                              ('mbange', 'mbange@example.com', null),
                                              ('mbovemum', 'mbovemum@example.com', 2),
                                              ('ando', 'ando@example.com', 2);
+
+-- Use JOIN, LEFT JOIN, and RIGHT JOIN to combine results from the users and roles tables as we did in the lesson.
+-- Before you run each query, guess the expected number of results.
+
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+         JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name as user_name, users.email, roles.name as role_name
+FROM users
+         LEFT JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name as user_name, users.email, roles.name as role_name
+FROM users
+         RIGHT JOIN roles ON users.role_id = roles.id;
+
+-- Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries.
+-- Use COUNT and the appropriate join type to get a list of roles along with the number of users that have a given role.
+-- Hint: You will also need to use GROUP BY in the query.
+
+SELECT roles.name as role_name, count(users.name) as no_of_users
+FROM users
+         JOIN roles ON users.role_id = roles.id
+GROUP BY role_name;
